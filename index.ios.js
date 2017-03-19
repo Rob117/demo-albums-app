f2118e013af571598f1b2f1a4818997c1415dab6
Robert Sherling <rob@robsherling.com>
2017-03-19 16:36:46 -0400

    Add boilerplate for Album List comp

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,18 @@
 // Import library to help create Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header'
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header'
+import AlbumList from './src/components/AlbumList'
 
 // create Component. Styling goes inside
-const App = () => {
-  return (
-    <Header headerText={'Albums'}/>
-  );
-};
+// We wrap all of our components in a view tag because we can only return
+// a single top-level object
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
 
 // render component to device
 AppRegistry.registerComponent('albums', () => App);
